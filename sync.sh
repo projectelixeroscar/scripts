e24fd6b7a1c0b7a3d9205f45237edc50c6bb55ef
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-09 15:48:38 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -17,10 +17,15 @@ rm -rf vendor/oplus/camera
 rm -rf vendor/oneplus/sm6375-common
 rm -rf hardware/oplus
 rm -rf hardware/custom/interfaces
+
 #Clone trees local manifest
 git clone https://github.com/PixelOS-AOSP/hardware_custom_interfaces.git hardware/custom/interfaces
-git clone https://github.com/projectelixeroscar/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$
+git clone https://github.com/projectelixeroscar/device_oneplus_oscar.git device/oneplus/oscar
+git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+git clone https://github.com/projectelixeroscar/android_kernel_oneplus_sm8350.git kernel/oneplus/sm6375
+git clone https://github.com/projectelixeroscar/vendor_oneplus_oscar.git vendor/oneplus/oscar
+git clone https://github.com/projectelixeroscar/vendor_oneplus_sm6375-common.git vendor/oneplus/sm6375-common
+git clone https://github.com/projectelixeroscar/hardware_oplus.git hardware/oplus
 #remove rm -rf hardware/google/pixel/kernel_headers
 git clone https://www.opencode.net/romdumprx/oplus_camera.git -b 14.0 vendor/oplus/camera
 rm -rf hardware/google/pixel/kernel_headers
