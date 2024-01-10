2b08ba1ba4bc7d53e1fd81bbccc90ba1fc7693e3
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-10 17:01:48 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -17,7 +17,7 @@ echo
 
 #rm -rf .repo/local_manifests
 #rm -rf device/oneplus/oscar
-#rm -rf device/oneplus/sm6375-common
+rm -rf device/oneplus/sm6375-common
 #rm -rf kernel/oneplus/sm6375
 #rm -rf vendor/oneplus/oscar
 #rm -rf vendor/oplus/camera
@@ -30,7 +30,7 @@ echo
 #Clone trees local manifest
 #git clone https://github.com/PixelOS-AOSP/hardware_custom_interfaces.git hardware/custom/interfaces
 #git clone https://github.com/projectelixeroscar/device_oneplus_oscar.git device/oneplus/oscar
-#git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
 #git clone https://github.com/projectelixeroscar/android_kernel_oneplus_sm8350.git kernel/oneplus/sm6375
 #git clone https://github.com/projectelixeroscar/vendor_oneplus_oscar.git vendor/oneplus/oscar
 #git clone https://github.com/projectelixeroscar/vendor_oneplus_sm6375-common.git vendor/oneplus/sm6375-common
