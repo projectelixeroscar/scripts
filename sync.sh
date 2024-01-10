2e8915916d12d59b67505bc66d62015ad8f26f97
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-10 09:01:48 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -10,7 +10,7 @@ echo
 #Remove local manifest
 rm -rf .repo/local_manifests
 #rm -rf device/oneplus/oscar
-#rm -rf device/oneplus/sm6375-common
+rm -rf device/oneplus/sm6375-common
 #rm -rf kernel/oneplus/sm6375
 #rm -rf vendor/oneplus/oscar
 #rm -rf vendor/oplus/camera
@@ -18,12 +18,12 @@ rm -rf .repo/local_manifests
 #rm -rf hardware/oplus
 #rm -rf hardware/custom/interfaces
 #rm -rf bootable/recovery
-git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+#git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
 #git clone https://github.com/DerpFest-AOSP/bootable_recovery.git bootable/recovery
 #Clone trees local manifest
 #git clone https://github.com/PixelOS-AOSP/hardware_custom_interfaces.git hardware/custom/interfaces
 #git clone https://github.com/projectelixeroscar/device_oneplus_oscar.git device/oneplus/oscar
-#git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
 #git clone https://github.com/projectelixeroscar/android_kernel_oneplus_sm8350.git kernel/oneplus/sm6375
 #git clone https://github.com/projectelixeroscar/vendor_oneplus_oscar.git vendor/oneplus/oscar
 #git clone https://github.com/projectelixeroscar/vendor_oneplus_sm6375-common.git vendor/oneplus/sm6375-common
