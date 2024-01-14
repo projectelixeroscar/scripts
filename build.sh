ab84f3dd0d5ddee0bcfc4d6afa64f3b6b1392a4c
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-14 05:44:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #envsetup
 source build/envsetup.sh
-make clean
 # lunch command 
 lunch aosp_oscar-user
 export TZ=Asia/Dhaka
 #make command
-mka bacon
+mka bacon -j16
 # Check build status and display animation
