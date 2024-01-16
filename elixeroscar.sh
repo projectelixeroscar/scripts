9b4f99c28846ef240df8639c8cbb97760bce5605
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-16 13:34:31 +0530

    Update elixeroscar.sh

--- elixeroscar.sh
+++ elixeroscar.sh
@@ -23,9 +23,13 @@ echo " remove local manifest "
 rm -rf .repo/local_manifests
 echo " Clone trees local manifest "
 #Clone trees local manifest
-git clone https://github.com/projectelixeroscar/local_manifest.git --depth 1 -b main .repo/local_manifests
-echo " sync the trees local manifest "
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/projectelixeroscar/device_oneplus_oscar.git device/oneplus/oscar
+git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+git clone https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/projectelixeroscar/android_kernel_oneplus_sm8350.git kernel/oneplus/sm6375
+git clone https://github.com/projectelixeroscar/vendor_oneplus_oscar.git vendor/oneplus/oscar
+git clone https://github.com/projectelixeroscar/vendor_oneplus_sm6375-common.git vendor/oneplus/sm6375-common
+git clone https://github.com/projectelixeroscar/hardware_oplus.git hardware/oplus
 echo " hardware/google/pixel/kernel_headers "
 rm -rf hardware/google/pixel/kernel_headers
 
