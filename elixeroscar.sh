a4115440da6625b4e7a69becd1e65e07f749bc07
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-10 16:14:36 +0530

    Update elixeroscar.sh

--- elixeroscar.sh
+++ elixeroscar.sh
@@ -26,13 +26,21 @@ echo " remove local manifest "
 #rm -rf .repo/local_manifests
 echo " Clone trees local manifest "
 #Clone trees local manifest
+rm -rf device/oneplus/oscar
 git clone https://github.com/projectelixeroscar/device_oneplus_oscar.git device/oneplus/oscar
+rm -rf device/oneplus/sm6375-common
 git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+rm -rf prebuilts/clang/host/linux-x86/clang-proton
 git clone https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+rm -rfkernel/oneplus/sm6375
 git clone https://github.com/projectelixeroscar/android_kernel_oneplus_sm8350.git kernel/oneplus/sm6375
+rm -rf vendor/oneplus/oscar
 git clone https://github.com/projectelixeroscar/vendor_oneplus_oscar.git vendor/oneplus/oscar
+rm -rf vendor/oneplus/sm6375-common
 git clone https://github.com/projectelixeroscar/vendor_oneplus_sm6375-common.git vendor/oneplus/sm6375-common
+rm -rf hardware/oplus
 git clone https://github.com/projectelixeroscar/hardware_oplus.git hardware/oplus
+rm -rf vendor/custom-preference
 git clone https://github.com/projectelixeroscar/vendor_custom-preference.git -b UNO vendor/custom-preference
 echo " hardware/google/pixel/kernel_headers "
 rm -rf hardware/google/pixel/kernel_headers
