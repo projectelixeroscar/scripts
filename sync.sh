63da2186a99e20faa421d57bbde68481258a4e9f
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-11 20:02:04 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -17,7 +17,7 @@ echo
 
 #rm -rf .repo/local_manifests
 #rm -rf device/oneplus/oscar
-rm -rf device/oneplus/sm6375-common
+#rm -rf device/oneplus/sm6375-common
 #rm -rf system/extras
 #rm -rf kernel/oneplus/sm6375
 #rm -rf vendor/oneplus/oscar
@@ -29,7 +29,7 @@ rm -rf device/oneplus/sm6375-common
 #Clone trees local manifest
 #git clone https://github.com/Evolution-X/system_extras.git system/extras
 #git clone https://github.com/projectelixeroscar/device_oneplus_oscar.git device/oneplus/oscar
-git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+#git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
 #git clone https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
 #git clone https://github.com/projectelixeroscar/android_kernel_oneplus_sm8350.git kernel/oneplus/sm6375
 #git clone https://github.com/projectelixeroscar/vendor_oneplus_oscar.git vendor/oneplus/oscar
@@ -38,7 +38,7 @@ git clone https://github.com/projectelixeroscar/device_oneplus_sm6375-common.git
 #remove rm -rf hardware/google/pixel/kernel_headers
 #git clone https://gitlab.com/Mrick343/proprietary_vendor_oplus_camera.git -b 13.0 vendor/oplus/camera
 #rm -rf hardware/google/pixel/kernel_headers
-rm -rf vendor/qcom/opensource/vibrator
+#rm -rf vendor/qcom/opensource/vibrator
 #rm -rf vendor/aosp
 #git clone https://github.com/projectelixeroscar/vendor_aosp.git vendor/aosp
 #git clone https://github.com/projectelixeroscar/vendor_pixel-framework.git vendor/pixel-framework
